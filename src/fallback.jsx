e8f05b4307bd617ae652f3a78f861e664af3ff58
tbd <hoytt2025@gmail.com>
2026-02-26 02:11:14 +0000

    Under construction (did i do this wrong?)

--- src/fallback.jsx
+++
@@ -1,7 +0,0 @@
-export default function Fallback() {
-  return (
-    <div className="fixed inset-0 z-9999 flex items-center justify-center bg-[#050913] text-white/80">
-      <h1 className="text-2xl font-semibold">Loading...</h1>
-    </div>
-  );
-}
